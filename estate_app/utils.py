761d21a76df3a7d6f026c1fd8157fae909e8ce82
mymi14s <mymi14s@hotmail.com>
2021-04-14 09:25:58 +0100

    Web Search Engine

--- estate_app/utils.py
+++ estate_app/utils.py
@@ -13,9 +13,8 @@ def sendmail(doc, recipients, msg, title, attachments=None):
     frappe.enqueue(method=frappe.sendmail, queue='short', timeout=300, **email_args)
 
 
-def paginate(doctype, page=0, paginate_by=6):
-    prev, next = 0, 0
-    conditions = " "
+def paginate(doctype, page=0, conditions=" ", paginate_by=6):
+    prev, next, search = 0, 0, False
     query = f"""SELECT name, property_name, status, address, grand_total,
                 image FROM `tab{doctype}` {conditions} ORDER BY creation DESC """
 
@@ -28,16 +27,19 @@ def paginate(doctype, page=0, paginate_by=6):
         else:
             prev, next = page-1, 0
     else:
-        count = frappe.db.sql(f"""SELECT COUNT(name) as count FROM `tab{doctype}`;""", as_dict=True)[0].count
+        count = frappe.db.sql(f"""SELECT COUNT(name) as count FROM `tab{doctype}` {conditions};""", as_dict=True)[0].count
         if(count>paginate_by):
             prev, next = 0, 2
         else:
             pass
         properties = frappe.db.sql(query+f"""LIMIT {paginate_by};""", as_dict=True)
+
+    if(conditions):search=True
     return {
         'properties': properties,
         'prev': prev,
         'next': next,
+        'search':search,
     }
 
 
--- estate_app/www/property/index.py
+++ estate_app/www/property/index.py
@@ -4,8 +4,19 @@ from estate_app.utils import paginate
 
 def get_context(context):
     page = frappe.form_dict.page
-    pagination = paginate('Property', page)
+    # check if search request
+    conditions = " "
+    type, status, city = frappe.form_dict.type, frappe.form_dict.status, frappe.form_dict.city
+    if(type and status and city):
+        conditions = f"""WHERE property_type='{type}' AND city='{city}' AND status='{status}'"""
+        context.type = type
+        context.status = status
+        context.city = city
+    pagination = paginate(doctype='Property', page=page, conditions=conditions) #pass to pagination
+    context.cities = frappe.db.sql("""SELECT name FROM `tabCity`;""", as_dict=True)
+    context.types = frappe.db.sql("""SELECT name FROM `tabProperty Type`;""", as_dict=True)
     context.properties = pagination.get('properties')
+    context.search = pagination.get('search')
     context.prev = pagination.get('prev')
     context.next = pagination.get('next')
 
