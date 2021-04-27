78ea9c617a338869a6365ff1b5fa71300b80f01b
mymi14s <mymi14s@hotmail.com>
2021-04-27 02:31:19 +0100

    webhook

--- estate_app/www/agents/index.py
+++ estate_app/www/agents/index.py
@@ -2,10 +2,5 @@ import frappe
 
 def get_context(context):
     context.name = 'Ghorz'
-    context.car = 'Dodge'
-    frappe.session.data['test'] = 'hi'
-    frappe.session['test'] = 'hello'
-    frappe.form_dict['test'] = 'hello'
-    print(f"\n\n\n{frappe.session}\n\n\n")
-    context._data = frappe.session.data
+    print(f"\n\n\n{frappe.form_dict}\n\n\n")
     return context
